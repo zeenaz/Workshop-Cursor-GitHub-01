d28cf73c676fbb54fda7736a79a69b3d4cf2186f
Zeena <zhangyujie258@gmail.com>
2025-03-26 14:41:11 +1100

    为 SampleScene 添加了新的组件，调整了现有对象的局部位置和比例，并加入了 Rigidbody2D 设置以改进物理交互。

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float jumpForce = 5f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input from both A-D keys and arrow keys
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        
+        // Apply force to move the player
+        Vector2 force = new Vector2(moveInput * moveSpeed, 0f);
+        rb.AddForce(force, ForceMode2D.Force);
+
+        // Jump when space is pressed and player is grounded
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    // Check if player is grounded
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
